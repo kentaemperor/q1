289af93dc61de0da66e38fb185721198e13bb7f9
kenta <bzmrchildren@icloud.com>
2023-07-11 20:53:07 +0900

    first-commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-  /**
-  * Seed the application's database.
-  *
-  * @return void
-  */
+/**
+* Seed the application's database.
+*
+* @return void
+*/
   public function run()
   {
-    \App\Models\Author::factory(10)->create();
+     \App\Models\Author::factory(10)->create();
   }
 }
